015335cb75511236fd1195cd61837e7d7929613f
jiro-max <2576545647@qq.com>
2022-04-20 11:11:47 +0800

    Initial Commit

---
+++ pages/detail/detail.js
@@ -0,0 +1,90 @@
+/*const db = wx.cloud.database()
+
+const goods_col = db.collection('goods')
+
+Page({
+  data :{
+    detail : {}
+  },
+  onLoad(options){
+
+    let { id } = options
+
+    this.loadDetailData(id)
+  },
+  // 加载详情数据
+  async loadDetailData(id){
+
+    // 拿到数据库的商品
+    let ins = goods_col.doc(id)
+
+    //+1
+    await ins.update({
+      data :{
+        count : db.command.inc(1)
+      }
+    })
+
+    // 获取
+   let res =  await ins.get()
+
+   // 赋值
+   this.setData({
+     detail : res.data
+   })
+
+
+
+    // count +1
+    //  await goods_col.doc(id).update({
+    //   data : {
+    //     count : db.command.inc(1)
+    //   }
+    // })
+
+    // // 获取详情数据
+    // let res = await goods_col.doc(id).get()
+    // console.log('商品信息',res)
+    // this.setData({
+    //   detail : res.data
+    // })
+
+  }
+})*/
+const db = wx.cloud.database()
+const goods_col = db.collection('goods')
+Page({
+  data : {
+    detail : {}
+  },
+  onLoad(options){
+    let { id } =options
+    console.log('id',id)
+    this.loadDetailData(id)
+  },
+  async loadDetailData(id){//加载数据详情
+    let ins = goods_col.doc(id) //拿到数据库商品
+    await ins.update({//累计
+      data: {
+        count : db.command.inc(1)
+      }
+    })
+    let res =await ins.get() //获取
+    this.setData({//赋值
+      detail : res.data
+    })
+    /*//count+1
+    await goods_col.doc(id).update({
+      data :{
+        count :db.command.inc(1)
+      }
+    })
+
+    //获取详情数据
+    let res = await goods_col.doc(id).get()
+    console.log('商品信息', res)
+    this.setData({
+      detail:res.data
+    })*/
+  }
+})
